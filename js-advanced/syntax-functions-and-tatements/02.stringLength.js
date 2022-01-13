07e4cc0ef47691e838a28283a7348a77a2f206d7
AndriqnAngelov <andriqnangelov@gmail.com>
2022-01-13 09:42:25 +0200

    Delete 02.stringLength.js

--- js-advanced/syntax-functions-and-tatements/02.stringLength.js
+++
@@ -1,9 +0,0 @@
-function solved(inputOne, inputTwo, inputThree) {
-  let lengthOfInputs = Number(
-    inputOne.length + inputTwo.length + inputThree.length
-  );
-  let averageLentgh = Math.floor(lengthOfInputs / 3);
-  console.log(lengthOfInputs);
-  console.log(averageLentgh);
-}
-
