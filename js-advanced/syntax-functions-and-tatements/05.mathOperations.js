3768c91525fa8cdc4dd49bf3a2a9035ec31630e8
AndriqnAngelov <andriqnangelov@gmail.com>
2022-01-13 09:40:02 +0200

    Create 05.mathOperations.js

---
+++ js-advanced/syntax-functions-and-tatements/05.mathOperations.js
@@ -0,0 +1,17 @@
+function solved(numOne, numTwo, operator) {
+  let result = 0;
+  if (operator == "+") {
+    result = numOne + numTwo;
+  } else if (operator == "-") {
+    result = numOne - numTwo;
+  } else if (operator == "/") {
+    result = numOne / numTwo;
+  } else if (operator == "*") {
+    result = numOne * numTwo;
+  } else if (operator == "%") {
+    result = numOne % numTwo;
+  } else if (operator == "**") {
+    result = numOne ** numTwo;
+  }
+  console.log(result);
+}
