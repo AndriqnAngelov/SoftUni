f1284b02397da62cb6e13128ed84a6c9e7846344
AndriqnAngelov <andriqnangelov@gmail.com>
2022-01-14 14:59:07 +0200

    Update 01.fruit.js

--- js-advanced/syntax-functions-and-statements-exercise/01.fruit.js
+++ js-advanced/syntax-functions-and-statements-exercise/01.fruit.js
@@ -1,9 +1,9 @@
 function solved(fruit, weight, priceOfKilogram) {
   let fruitType = fruit;
   let kilograms = weight / 1000;
-  let price = kilograms * priceOfKilogram;
+  let money = kilograms * priceOfKilogram;
   console.log(
-    `I need $${price.toFixed(2)} to buy ${kilograms.toFixed(
+    `I need $${money.toFixed(2)} to buy ${kilograms.toFixed(
       2
     )} kilograms ${fruitType}.`
   );
