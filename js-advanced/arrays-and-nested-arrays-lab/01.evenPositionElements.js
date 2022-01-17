c8f8a290501efb76b1fb642100ce28d7055d6bec
AndriqnAngelov <andriqnangelov@gmail.com>
2022-01-17 12:12:59 +0200

    Update 01.evenPositionElements.js

--- js-advanced/arrays-and-nested-arrays-lab/01.evenPositionElements.js
+++ js-advanced/arrays-and-nested-arrays-lab/01.evenPositionElements.js
@@ -1,9 +1,7 @@
 function solved(arrInput) {
   let result = "";
-  for (let i = 0; i < arrInput.length; i++) {
-    if (i % 2 == 0) {
-      result += arrInput[i] + " ";
-    }
+  for (let i = 0; i < arrInput.length; i += 2) {
+    result += arrInput[i] + " ";
   }
   console.log(result);
 }
