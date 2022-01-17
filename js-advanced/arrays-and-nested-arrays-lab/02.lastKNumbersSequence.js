d6ba34395623b8d622e7d4c277a367299941d18b
AndriqnAngelov <andriqnangelov@gmail.com>
2022-01-17 15:44:11 +0200

    Create 02.lastKNumbersSequence.js

---
+++ js-advanced/arrays-and-nested-arrays-lab/02.lastKNumbersSequence.js
@@ -0,0 +1,15 @@
+function solved(n, k) {
+  let result = [1];
+  for (let i = 1; i < n; i++) {
+    let sum = 0;
+    let m = k;
+    if (result.length < k) {
+      m = result.length;
+    }
+    for (let x = 0; x < m; x++) {
+      sum += result[i - 1 - x];
+    }
+    result.push(sum);
+  }
+  return result;
+}
