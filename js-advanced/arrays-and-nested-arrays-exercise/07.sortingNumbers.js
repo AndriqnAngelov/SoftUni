f96de340f99b421e5153af2311d2554df6df345c
AndriqnAngelov <andriqnangelov@gmail.com>
2022-01-20 21:24:58 +0200

    Create 07.sortingNumbers.js

---
+++ js-advanced/arrays-and-nested-arrays-exercise/07.sortingNumbers.js
@@ -0,0 +1,10 @@
+function solved(array) {
+    array.sort((a, b) => a - b);
+    let result = [];
+    while (array.length != 0) {
+        result.push(array.shift());
+        result.push(array.pop());
+    }
+    return result;
+  }
+
